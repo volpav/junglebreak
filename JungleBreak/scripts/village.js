4fe7d18bf1b9004f8f9c9462cc1a43df9f0bc92e
Pavel Volgarev <volpav@gmail.com>
2014-02-01 14:08:56 +0100

    Village (terrain).

--- JungleBreak/scripts/village.js
+++ JungleBreak/scripts/village.js
@@ -3,7 +3,7 @@ var Village = {
 		Q.scene('village', function (stage) {
         	var background = new Q.TileLayer({ 
 				dataAsset: 'village.tmx', 
-				layerIndex: 1, 
+				layerIndex: 2, 
 				sheet: 'village.tiles.background', 
 				tileW: 32, 
 				tileH: 32, 
@@ -12,20 +12,31 @@ var Village = {
 
             stage.insert(background);
 
-			var collision = new Q.TileLayer({ 
+            var grass = new Q.TileLayer({ 
+				dataAsset: 'village.tmx', 
+				layerIndex: 1, 
+				sheet: 'village.tiles.terrain', 
+				tileW: 32, 
+				tileH: 32, 
+				type: Q.SPRITE_NONE 
+			});
+
+			stage.insert(grass);
+
+			var collision = new Q.TileLayer({
 				dataAsset: 'village.tmx', 
 				layerIndex: 0, 
 				sheet: 'village.tiles.terrain', 
 				tileW: 32, 
-				tileH: 32 
+				tileH: 32
 			});
 
 			stage.collisionLayer(collision);
 	    });
 
-		Q.load(['village.terrain.png', 'village.tmx'], function () {
+		Q.load(['village.terrain.png', 'village.house.png', 'village.tmx'], function () {
+            Q.sheet('village.tiles.background', 'village.house.png', { tilew: 32, tileh: 32 });
             Q.sheet('village.tiles.terrain', 'village.terrain.png', { tilew: 32, tileh: 32 });
-            Q.sheet('village.tiles.background', 'village.terrain.png', { tilew: 32, tileh: 32 });
 
             complete();
         });
