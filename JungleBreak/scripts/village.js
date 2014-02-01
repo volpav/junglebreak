43cf0252ba88d034a2621c567d7067e1e666f266
Pavel Volgarev <volpav@gmail.com>
2014-02-01 14:32:39 +0100

    Village.

--- JungleBreak/scripts/village.js
+++ JungleBreak/scripts/village.js
@@ -1,5 +1,21 @@
 var Village = {
 	load: function(Q, complete) {
+		Q.Sprite.extend('Player', {
+		    init: function (p) {
+		        this._super(p, { asset: 'male_traveler_idle.gif', x: 500, y: 300, jumpSpeed: -380 });
+		        this.add('2d, platformerControls');
+		    },
+
+		    step: function (dt) {
+		        if (Q.inputs['left'] && this.p.direction == 'right') {
+		            this.p.flip = 'x';
+		        }
+		        if (Q.inputs['right'] && this.p.direction == 'left') {
+		            this.p.flip = false;
+		        }
+		    }
+		});
+
 		Q.scene('village', function (stage) {
         	var background = new Q.TileLayer({ 
 				dataAsset: 'village.tmx', 
@@ -23,6 +39,17 @@ var Village = {
 
 			stage.insert(grass);
 
+			var houses = new Q.TileLayer({ 
+				dataAsset: 'village.tmx', 
+				layerIndex: 3, 
+				sheet: 'village.tiles.houses', 
+				tileW: 32, 
+				tileH: 32, 
+				type: Q.SPRITE_NONE 
+			});
+
+			stage.insert(houses);
+
 			var collision = new Q.TileLayer({
 				dataAsset: 'village.tmx', 
 				layerIndex: 0, 
@@ -31,14 +58,23 @@ var Village = {
 				tileH: 32
 			});
 
+			var player = stage.insert(new Q.Player());
+
+			stage.add('viewport').follow(player, {
+				x: true, 
+				y: false
+			});
+
 			stage.collisionLayer(collision);
 	    });
 
-		Q.load(['village.terrain.png', 'village.house.png', 'village.tmx'], function () {
+		Q.load(['village.terrain.png', 'village.house.png', 'village.tmx', 'male_traveler_idle.gif'], function () {
             Q.sheet('village.tiles.background', 'village.house.png', { tilew: 32, tileh: 32 });
             Q.sheet('village.tiles.terrain', 'village.terrain.png', { tilew: 32, tileh: 32 });
+            Q.sheet('village.tiles.houses', 'village.house.png', { tilew: 32, tileh: 32 });
 
             complete();
         });
 	}
 };
+
